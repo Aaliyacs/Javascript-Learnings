d75024c5450a72b622e6c7166c4bb58b7fd5ea7b
Aaliyasiddique <aaliyaasiddique@gmail.com>
2026-06-15 18:37:20 +0000

    container config done

---
+++ variables/Datatype.js
@@ -0,0 +1,9 @@
+//string
+let name = "Aaliya"
+//number
+let age = 20
+//boolean
+let isStudent = true
+console.log(typeof age)
+console.log(typeof null)
+// console.table([name, age, isStudent])
---
+++ variables/variable.js
@@ -0,0 +1,5 @@
+let score = false
+let valueInNumber = Number(score)
+console.log(typeof score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
