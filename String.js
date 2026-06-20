40f6c843e34ee762e2e67def78e48bb73a16ce62
Aaliyasiddique <aaliyaasiddique@gmail.com>
2026-06-20 20:52:44 +0000

    Strings in javascript

---
+++ String.js
@@ -0,0 +1,19 @@
+let name = 'Aaliya';
+let age = 21;
+console.log(`Hello my name is ${name} and my age is ${age}`);
+const gameName = new String("PlayStation");
+console.log(gameName.length);
+console.log(gameName.charAt(3));
+console.log(gameName.indexOf("y"));
+console.log(gameName.toUpperCase());
+console.log(gameName.toLowerCase());
+const newString = gameName.substring(0,5);
+console.log(newString);
+const anotherString = gameName.slice(-7,7);
+console.log(anotherString);
+const secondString = "   Aaliyaa   "
+console.log(secondString);
+console.log(secondString.trim());
+const url = "https:\\aaliyaa@google.com"
+console.log(url.replace("google","microsoft"));
+console.log(url.includes("aaliyaa"))
