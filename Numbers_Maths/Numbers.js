ff65b99b74611bb8cf826781dde8a000cbdc5cbb
Aaliyasiddique <aaliyaasiddique@gmail.com>
2026-06-26 21:46:12 +0000

    Numbers and maths in javascript

---
+++ Numbers_Maths/Numbers.js
@@ -0,0 +1,14 @@
+const score = 100;
+console.log(score);
+const age = new Number(21);
+console.log(age);
+console.log(age.toString().length);
+
+console.log(age.toFixed(2));
+const result = 123.456;
+console.log(result.toFixed(2));
+console.log(result.toPrecision(2));
+const hundreds = 10000;
+console.log(hundreds.toLocaleString());
+console.log(Math.max(11,32,12,21,100));
+console.log(Math.min(11,32,12,21,100));
---
+++ Numbers_Maths/maths.js
@@ -0,0 +1,11 @@
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.6));
+console.log(Math.floor(4.6));
+console.log(Math.random());
+console.log(Math.random()*10+1);
+console.log(Math.floor(Math.random()*10)+1);
+const max = 20;
+const min = 10;
+console.log(Math.floor(Math.random() * (max - min +1))+ min)
